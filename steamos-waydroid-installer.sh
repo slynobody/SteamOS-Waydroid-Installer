fecf52e8173bebcb03d87c9d2a747a7660fde02c
slynobody <55579266+slynobody@users.noreply.github.com>
2024-05-26 20:06:59 +0000

    Update steamos-waydroid-installer.sh

--- steamos-waydroid-installer.sh
+++ steamos-waydroid-installer.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
-
 clear
+
+sudo steamos-readonly disable
+sudo pacman-key --init
+sudo pacman-key --populate archlinux
+sudo pacman-key --populate holo
+sudo pacman -Syu
+pacman -S fakeroot dkms
+#get & install latest headers, 'main'-context (next supposedly would read *valve8*-something)
+wget https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-main/os/x86_64/linux-neptune-65-headers-6.5.0.valve7-1-x86_64.pkg.tar.zst
+sudo pacman -U ./linux-neptune-65-headers-6.5.0.valve7-1-x86_64.pkg.tar.zst
+#get & install latest binder-dkms (which builds the missing kernel-module)
+git clone https://aur.archlinux.org/binder_linux-dkms.git
+cd binder_linux-dkms
+makepkg
+sudo pacman -U ./binder_linux-dkms-6.8-1-x86_64.pkg.tar.zst
+#offer the kernel-module to the sd-waydroid-installer (one has to adapt the script to include 3.7 latest: 65-gbb001cd639ba)
+mv ./binder/6.5.0-valve7-1-neptune-65-g6efe817cc486 ./binder/6.5.0-valve7-1-neptune-65-gbb001cd639ba/
+sudo cp /usr/lib/modules/6.5.0-valve7-1-neptune-65-gbb001cd639ba/updates/dkms/binder_linux.ko.zst ./binder/6.5.0-valve7-1-neptune-65-gbb001cd639ba/
+sudo steamos-readonly enable
 
 echo SteamOS Waydroid Installer Script by ryanrudolf
 echo https://github.com/ryanrudolfoba/SteamOS-Waydroid-Installer
