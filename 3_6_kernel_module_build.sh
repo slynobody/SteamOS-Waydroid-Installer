056f791ef0e5521735e5627b62efd4645561dbc0
slynobody <55579266+slynobody@users.noreply.github.com>
2024-07-02 21:00:08 +0000

    Update and rename steamos-waydroid-installer-3.6 to 3_6_kernel_module_build.sh

---
+++ 3_6_kernel_module_build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+clear
+
+sudo steamos-readonly disable
+sudo pacman-key --init
+sudo pacman-key --populate archlinux
+sudo pacman-key --populate holo
+sudo pacman -Syu
+sudo pacman -S fakeroot dkms
+#get & install latest headers, 'main'-context (next supposedly would read *valve13*-something)
+
+wget https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-3.6/os/x86_64/linux-neptune-65-headers-6.5.0.valve12-1-x86_64.pkg.tar.zst
+sudo pacman -U ./linux-neptune-65-headers-6.5.0.valve12-1-x86_64.pkg.tar.zst
+
+#get & install latest binder-dkms (which builds the missing kernel-module)
+git clone https://aur.archlinux.org/binder_linux-dkms.git
+cd binder_linux-dkms
+makepkg
+sudo pacman -U ./binder_linux-dkms-6.8-1-x86_64.pkg.tar.zst
+
+cd ..
+#offer the kernel-module to the sd-waydroid-installer (one has to adapt the script to include latest 3.6: g1889664e19fc )
+mkdir binder/6.5.0-valve12-1-neptune-65-g1889664e19fc 
+sudo cp /usr/lib/modules/6.5.0-valve12-1-neptune-65-g1889664e19fc/updates/dkms/binder_linux.ko.zst binder/6.5.0-valve12-1-neptune-65-g1889664e19fc
+
+#remove old packages (nec for updating with higher kernel version later on)
+sudo pacman -R fakeroot dkms binder_linux-dkms linux-neptune-65-headers
+sudo steamos-readonly enable
+
+
