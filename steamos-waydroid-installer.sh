3ac27e1311a92d06c15355850cc041fbe6e17bef
slynobody <55579266+slynobody@users.noreply.github.com>
2024-07-02 11:02:46 +0000

    latest main

--- steamos-waydroid-installer.sh
+++ steamos-waydroid-installer.sh
@@ -7,18 +7,23 @@ sudo pacman-key --populate archlinux
 sudo pacman-key --populate holo
 sudo pacman -Syu
 sudo pacman -S fakeroot dkms
+
 #get & install latest headers, 'main'-context (next supposedly would read *valve8*-something)
-wget https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-main/os/x86_64/linux-neptune-65-headers-6.5.0.valve12-1-x86_64.pkg.tar.zst
-sudo pacman -U ./linux-neptune-65-headers-6.5.0.valve12-1-x86_64.pkg.tar.zst
+wget https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-main/os/x86_64/linux-neptune-65-headers-6.5.0.valve13-1-x86_64.pkg.tar.zst
+sudo pacman -U ./linux-neptune-65-headers-6.5.0.valve13-1-x86_64.pkg.tar.zst
+
 #get & install latest binder-dkms (which builds the missing kernel-module)
 git clone https://aur.archlinux.org/binder_linux-dkms.git
 cd binder_linux-dkms
 makepkg
 sudo pacman -U ./binder_linux-dkms-6.8-1-x86_64.pkg.tar.zst
+
 cd ..
-#offer the kernel-module to the sd-waydroid-installer (current latest: gbb001cd639ba)
-mkdir binder/6.5.0-valve12-1-neptune-65-g1889664e19fc
-sudo cp /usr/lib/modules/6.5.0-valve12-1-neptune-65-g1889664e19fc/updates/dkms/binder_linux.ko.zst binder/6.5.0-valve12-1-neptune-65-g1889664e19fc
+mkdir binder/6.5.0-valve13-1-neptune-65-gd5e176bdacb0
+#offer the kernel-module to the sd-waydroid-installer (one has to adapt the script to include 3.7 latest: 65-gd5e176bdacb0)
+sudo cp /usr/lib/modules/6.5.0-valve13-1-neptune-65-gd5e176bdacb0/updates/dkms/binder_linux.ko.zst binder/6.5.0-valve13-1-neptune-65-gd5e176bdacb0
+
+#remove now unnec packages (nec for future reinstalls!)
 sudo pacman -R fakeroot dkms binder_linux-dkms linux-neptune-65-headers
 sudo steamos-readonly enable
 
@@ -31,7 +36,7 @@ sleep 2
 kernel_version=$(uname -r)
 kernel1=6.1.52-valve9-1-neptune-61
 kernel2=6.1.52-valve14-1-neptune-61
-kernel3=6.1.52-valve16-1-neptune-61
+kernel3=6.5.0-valve13-1-neptune-65-gd5e176bdacb0
 kernel4=6.5.0-valve12-1-neptune-65-g1889664e19fc
 AUR_CASUALSNEK=https://github.com/casualsnek/waydroid_script.git
 AUR_CASUALSNEK2=https://github.com/ryanrudolfoba/waydroid_script.git
