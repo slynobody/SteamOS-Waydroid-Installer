c2d94e854f93134fa27c4205030a21256753224b
slynobody <55579266+slynobody@users.noreply.github.com>
2024-08-26 14:39:02 +0000

    Update 3_7_kernel_module_build.sh

--- 3_7_kernel_module_build.sh
+++ 3_7_kernel_module_build.sh
@@ -5,25 +5,25 @@ sudo steamos-readonly disable
 sudo pacman-key --init
 sudo pacman-key --populate archlinux
 sudo pacman-key --populate holo
-sudo yes '' | pacman -Syu
-sudo yes '' | pacman-key --populate holo
-sudo yes '' | pacman -S fakeroot dkms
+yes '' | sudo pacman -Syu
+yes '' | sudo pacman-key --populate holo
+yes '' | sudo pacman -S fakeroot dkms
 
 #get & install latest headers, 'main'-context (next supposedly would read *valve8*-something)
 wget https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-main/os/x86_64/linux-neptune-65-headers-6.5.0.valve19-1-x86_64.pkg.tar.zst
-sudo yes '' | pacman -U ./linux-neptune-65-headers-6.5.0.valve19-1-x86_64.pkg.tar.zst
+yes '' | sudo pacman -U ./linux-neptune-65-headers-6.5.0.valve19-1-x86_64.pkg.tar.zst
 
 #get & install latest binder-dkms (which builds the missing kernel-module)
 git clone https://aur.archlinux.org/binder_linux-dkms.git
 cd binder_linux-dkms
 makepkg
-sudo yes '' | pacman -U ./binder_linux-dkms-6.8-1-x86_64.pkg.tar.zst
+yes '' | sudo pacman -U ./binder_linux-dkms-6.8-1-x86_64.pkg.tar.zst
 
 cd ..
-mkdir binder/6.5.0-valve19-1-neptune-65-gcd4898a4f8e6 
+mkdir binder/6.5.0-valve19-1-neptune-65
 #offer the kernel-module to the sd-waydroid-installer (one has to adapt the script to include 3.7 latest: 65-gd5e176bdacb0)
-sudo cp /usr/lib/modules/6.5.0-valve19-1-neptune-65-gcd4898a4f8e6/updates/dkms/binder_linux.ko.zst binder/6.5.0-valve19-1-neptune-65-gcd4898a4f8e6 
+sudo cp /usr/lib/modules/6.5.0-valve19-1-neptune-65-gcd4898a4f8e6/updates/dkms/binder_linux.ko.zst binder/6.5.0-valve19-1-neptune-65
 
 #remove now unnec packages (nec for future reinstalls!)
-sudo yes '' | pacman -R fakeroot dkms binder_linux-dkms linux-neptune-65-headers
+yes '' | sudo  pacman -R fakeroot dkms binder_linux-dkms linux-neptune-65-headers
 sudo steamos-readonly enable
