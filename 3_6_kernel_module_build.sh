2c3c2c4b8e64e1319d2f9a431f27fb35d2a97caa
slynobody <55579266+slynobody@users.noreply.github.com>
2024-09-06 20:05:47 +0000

    Update 3_6_kernel_module_build.sh

--- 3_6_kernel_module_build.sh
+++ 3_6_kernel_module_build.sh
@@ -5,17 +5,18 @@ sudo steamos-readonly disable
 sudo pacman-key --init
 sudo pacman-key --populate archlinux
 sudo pacman-key --populate holo
-sudo yes '' | pacman -Syu
-sudo yes '' | pacman -S fakeroot dkms
+sudo pacman -Syu
+sudo pacman -S fakeroot dkms
 #get & install latest headers, 'main'-context (next supposedly would read *valve20-2*-something)
-wget https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-3.6/os/x86_64/linux-neptune-65-headers-6.5.0.valve19-1-x86_64.pkg.tar.zst
-sudo yes '' | pacman -U ./linux-neptune-65-headers-6.5.0.valve19-1-x86_64.pkg.tar.zst
+wget https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-3.6/os/x86_64/linux-neptune-65-headers-6.5.0.valve20-1-x86_64.pkg.tar.zst
+#wget https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-main/os/x86_64/linux-neptune-65-headers-6.5.0.valve19-1-x86_64.pkg.tar.zst
+sudo pacman -U ./linux-neptune-65-headers-6.5.0.valve20-1-x86_64.pkg.tar.zst
 
 #get & install latest binder-dkms (which builds the missing kernel-module)
 git clone https://aur.archlinux.org/binder_linux-dkms.git
 cd binder_linux-dkms
 makepkg -f
-sudo yes '' | pacman -U ./binder_linux-dkms-6.8-1-x86_64.pkg.tar.zst
+sudo pacman -U ./binder_linux-dkms-6.8-1-x86_64.pkg.tar.zst
 
 cd ..
 #offer the kernel-module to the sd-waydroid-installer (adapt the script to include your kernel -> <uname>)
@@ -23,5 +24,5 @@ mkdir binder/6.5.0-valve19-1-neptune-65
 sudo cp /usr/lib/modules/6.5.0-valve19-1-neptune-65-g8e4b171a9b33/updates/dkms/binder_linux.ko.zst binder/6.5.0-valve19-1-neptune-65
 
 #remove old packages (nec for updating with higher kernel version later on)
-sudo yes '' | pacman -R fakeroot dkms binder_linux-dkms linux-neptune-65-headers
+sudo pacman -R fakeroot dkms binder_linux-dkms linux-neptune-65-headers
 sudo steamos-readonly enable
