8dde7effafd0307d0ce5716063c30a29eeb2b6ea
slynobody <55579266+slynobody@users.noreply.github.com>
2024-09-08 17:27:04 +0000

    Update 3_7_kernel_module_build.sh

--- 3_7_kernel_module_build.sh
+++ 3_7_kernel_module_build.sh
@@ -5,10 +5,14 @@ sudo steamos-readonly disable
 sudo pacman-key --init
 sudo pacman-key --populate archlinux
 sudo pacman-key --populate holo
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo ">>>> dont update! ('n') <<<<"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 sudo pacman -Syu
 sudo pacman-key --populate holo
+echo "<<<<<<<<<<<<<"
 echo "<<<< 'y' >>>>"
+echo "<<<<<<<<<<<<<"
 udo pacman -S fakeroot dkms
 
 #get & install latest headers, 'main'-context (next supposedly would read *valve8*-something)
@@ -23,9 +27,9 @@ sudo pacman -U ./binder_linux-dkms-6.8-1-x86_64.pkg.tar.zst
 
 cd ..
 mkdir binder/6.8.0-valve2-1-neptune-68
-#offer the kernel-module to the sd-waydroid-installer (one has to adapt the script to include 3.7 latest: 65-gd5e176bdacb0)
+#offer the kernel-module to the sd-waydroid-installer (one has always to adapt the script to include 3.7 latest)
 sudo cp /usr/lib/modules/6.8.0-valve2-1-neptune-68-ga022b3966af7/updates/dkms/binder_linux.ko.zst binder/6.8.0-valve2-1-neptune-68
 
 #remove now unnec packages (nec for future reinstalls!)
-yes '' | sudo  pacman -R fakeroot dkms binder_linux-dkms linux-neptune-65-headers
+sudo  pacman -R fakeroot dkms binder_linux-dkms linux-neptune-68-headers
 sudo steamos-readonly enable
