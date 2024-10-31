c341dc81e6c6f30afe07b98522db89e6d12aab94
slynobody <55579266+slynobody@users.noreply.github.com>
2024-10-31 19:18:02 +0000

    Update 3_7_kernel_module_build.sh

--- 3_7_kernel_module_build.sh
+++ 3_7_kernel_module_build.sh
@@ -5,8 +5,10 @@ sudo steamos-readonly disable
 sudo pacman-key --init
 sudo pacman-key --populate archlinux
 sudo pacman-key --populate holo
+echo "<<<<<<<<<<<<<"
+echo "<<<< 'n' >>>>"
+echo "<<<<<<<<<<<<<"
 sudo pacman -Syu
-sudo pacman-key --populate holo
 echo "<<<<<<<<<<<<<"
 echo "<<<< 'y' >>>>"
 echo "<<<<<<<<<<<<<"
