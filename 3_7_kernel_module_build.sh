ffcca1f7ff69a8234e6f16ba73cd3d41f142fccb
slynobody <55579266+slynobody@users.noreply.github.com>
2024-11-26 16:29:31 +0000

    Update 3_7_kernel_module_build.sh

--- 3_7_kernel_module_build.sh
+++ 3_7_kernel_module_build.sh
@@ -15,8 +15,8 @@ echo "<<<<<<<<<<<<<"
 sudo pacman -S fakeroot dkms
 
 #get & install latest headers, 'main'-context (next supposedly would read *valve8*-something)
-sudo wget https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-main/os/x86_64/linux-neptune-68-headers-6.8.12.valve5-1-x86_64.pkg.tar.zst
-sudo pacman -U ./linux-neptune-68-headers-6.8.12.valve5-1-x86_64.pkg.tar.zst
+sudo wget https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-main/os/x86_64/linux-neptune-68-headers-6.8.12.valve7-1-x86_64.pkg.tar.zst
+sudo pacman -U ./linux-neptune-68-headers-6.8.12.valve7-1-x86_64.pkg.tar.zst
 
 #get & install latest binder-dkms (which builds the missing kernel-module)
 git clone https://aur.archlinux.org/binder_linux-dkms.git
@@ -25,9 +25,9 @@ makepkg
 sudo pacman -U ./binder_linux-dkms-6.8-1-x86_64.pkg.tar.zst
 
 cd ..
-mkdir binder/6.8.12-valve5-1-neptune-68
+mkdir binder/6.8.12-valve7-1-neptune-68
 #offer the kernel-module to the sd-waydroid-installer (one has always to adapt the script to include 3.7 latest)
-sudo cp /usr/lib/modules/6.8.12-valve5-1-neptune-68-g6ca122c26f2b/updates/dkms/binder_linux.ko.zst binder/6.8.12-valve5-1-neptune-68
+sudo cp /usr/lib/modules/6.8.12-valve7-1-neptune-68-g8c3c3d4f5307/updates/dkms/binder_linux.ko.zst binder/6.8.12-valve7-1-neptune-68
 
 #remove now unnec packages (nec for future reinstalls!)
 sudo  pacman -R fakeroot dkms binder_linux-dkms linux-neptune-68-headers
