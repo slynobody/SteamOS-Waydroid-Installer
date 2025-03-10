9478dc37ad996ea17a8c89a499957d1d5a3b2786
slynobody <55579266+slynobody@users.noreply.github.com>
2025-03-10 19:55:13 +0000

    Update 3_8_kernel_module_build.sh

--- 3_8_kernel_module_build.sh
+++ 3_8_kernel_module_build.sh
@@ -1,33 +1,21 @@
 #!/bin/bash
-clear
-
-sudo steamos-readonly disable
-sudo pacman-key --init
-sudo pacman-key --populate archlinux
-sudo pacman-key --populate holo
-#echo "<<<<<<<<<<<<<"
-#echo "<<<< 'n' >>>>"
-#echo "<<<<<<<<<<<<<"
-#sudo pacman -Syu
-echo "<<<<<<<<<<<<<"
-echo "<<<< 'y' >>>>"
-echo "<<<<<<<<<<<<<"
+sudo steamos-devmode enable --no-prompt
 sudo pacman -S fakeroot dkms dnsmasq lxc debugedit
 
 #get & install latest headers, 'main'-context
-sudo wget https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-main/os/x86_64/linux-neptune-611-headers-6.11.11.valve6-3-x86_64.pkg.tar.zst
-sudo pacman -U ./linux-neptune-611-headers-6.11.11.valve6-3-x86_64.pkg.tar.zst
+sudo wget https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-main/os/x86_64/linux-neptune-611-headers-6.11.11.valve7-1-x86_64.pkg.tar.zst
+sudo pacman -U ./linux-neptune-611-headers-6.11.11.valve7-1-x86_64.pkg.tar.zst
 
 #get & install latest binder-dkms (which builds the missing kernel-module)
 git clone https://aur.archlinux.org/binder_linux-dkms.git
 cd binder_linux-dkms
 makepkg
-sudo pacman -U ./binder_linux-dkms-6.8-1-x86_64.pkg.tar.zst
+sudo pacman -U ./binder_linux-dkms-6.14-1-x86_64.pkg.tar.zst
 
 cd ..
-mkdir binder/6.11.11-valve6-3-neptune-611
+mkdir binder/6.11.11-valve7-1-neptune-611
 #offer the kernel-module to the sd-waydroid-installer (one has always to adapt the script to include 3.8 latest)
-sudo cp /usr/lib/modules/6.11.11-valve6-3-neptune-611-g8b3a15f817f8/updates/dkms/binder_linux.ko.zst binder/6.11.11-valve6-3-neptune-611
+sudo cp /usr/lib/modules/6.11.11-valve7-1-neptune-611-g755d27235234/updates/dkms/binder_linux.ko.zst binder/6.11.11-valve7-1-neptune-611
 
 
 #remove now unnec packages (nec for future reinstalls!)
