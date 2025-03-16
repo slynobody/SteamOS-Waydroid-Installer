a9a9854aed0df57c88f870d8ed387597b5001ff4
slynobody <55579266+slynobody@users.noreply.github.com>
2025-03-16 12:28:19 +0000

    Update 3_6_kernel_module_build.sh

--- 3_6_kernel_module_build.sh
+++ 3_6_kernel_module_build.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 clear
 
-sudo steamos-readonly disable
-sudo pacman-key --init
-sudo pacman-key --populate archlinux
-sudo pacman-key --populate holo
-sudo pacman -Syu
+sudo steamos-devmode enable --no-prompt
 sudo pacman -S fakeroot dkms
-#get & install latest headers, 'main'-context (next supposedly would read *valve20-2*-something)
+#get & install latest headers, 'main'-context (next supposedly would read *valve24-1*-something)
 sudo wget https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-3.6/os/x86_64/linux-neptune-65-headers-6.5.0.valve24-1-x86_64.pkg.tar.zst
 sudo pacman -U ./linux-neptune-65-headers-6.5.0.valve24-1-x86_64.pkg.tar.zst
 
@@ -20,7 +16,7 @@ sudo pacman -U ./binder_linux-dkms-6.14-1-x86_64.pkg.tar.zst
 cd ..
 #offer the kernel-module to the sd-waydroid-installer (adapt the script to include your kernel -> <uname>)
 mkdir binder/6.5.0-valve24-1-neptune-65
-sudo cp /usr/lib/modules/6.5.0-valve24-1-neptune-65-xxxxxx/updates/dkms/binder_linux.ko.zst binder/6.5.0-valve24-1-neptune-65
+sudo cp /usr/lib/modules/$(uname -r)/updates/dkms/binder_linux.ko.zst binder/6.5.0-valve24-1-neptune-65
 
 #remove old packages (nec for updating with higher kernel version later on)
 sudo pacman -R fakeroot dkms binder_linux-dkms linux-neptune-65-headers
