8815bd0d2e35c7f91f93caf07b3e1ed421369b8f
slynobody <55579266+slynobody@users.noreply.github.com>
2025-03-16 11:58:33 +0000

    Create 3_7_kernel_module_build.sh

---
+++ 3_7_kernel_module_build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+sudo steamos-devmode enable --no-prompt
+sudo pacman -S fakeroot dkms dnsmasq lxc debugedit
+
+#get & install latest headers, 'main'-context
+sudo wget https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-main/os/x86_64/linux-neptune-611-headers-6.11.11.valve9-2-x86_64.pkg.tar.zst
+sudo pacman -U ./linux-neptune-611-headers-6.11.11.valve9-2-x86_64.pkg.tar.zst
+
+#get & install latest binder-dkms (which builds the missing kernel-module)
+git clone https://aur.archlinux.org/binder_linux-dkms.git
+cd binder_linux-dkms
+makepkg
+sudo pacman -U ./binder_linux-dkms-6.14-1-x86_64.pkg.tar.zst
+
+cd ..
+mkdir binder/6.11.11-valve9-2-neptune-611
+#offer the kernel-module to the sd-waydroid-installer (one has always to adapt the script to include 3.8 latest)
+sudo cp /usr/lib/modules/6.11.11-valve9-2-neptune-611-g9b073eb8166e/updates/dkms/binder_linux.ko.zst binder/6.11.11-valve9-2-neptune-611
+
+
+#remove now unnec packages (nec for future reinstalls!)
+sudo  pacman -R fakeroot dkms binder_linux-dkms linux-neptune-68-headers
+sudo steamos-readonly enable
+
+#./steamos-waydroid-installer.sh
