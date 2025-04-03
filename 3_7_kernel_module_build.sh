f2915ee4de7ca07456c6c3e9319eac84074df8d1
slynobody <55579266+slynobody@users.noreply.github.com>
2025-04-03 21:36:10 +0000

    Update 3_7_kernel_module_build.sh

--- 3_7_kernel_module_build.sh
+++ 3_7_kernel_module_build.sh
@@ -3,7 +3,7 @@ sudo steamos-devmode enable --no-prompt
 sudo pacman -S fakeroot dkms dnsmasq lxc debugedit
 
 #get & install latest headers, 'main'-context
-sudo wget https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-main/os/x86_64/linux-neptune-611-headers-6.11.11.valve10-1-x86_64.pkg.tar.zst
+sudo wget https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-3.7/os/x86_64/linux-neptune-611-headers-6.11.11.valve10-1-x86_64.pkg.tar.zst
 sudo pacman -U ./linux-neptune-611-headers-6.11.11.valve10-1-x86_64.pkg.tar.zst
 
 #get & install latest binder-dkms (which builds the missing kernel-module)
@@ -19,7 +19,7 @@ sudo cp /usr/lib/modules/6.11.11-valve10-1-neptune-611-gb69e902b4338/updates/dkm
 
 
 #remove now unnec packages (nec for future reinstalls!)
-sudo  pacman -R fakeroot dkms binder_linux-dkms linux-neptune-68-headers
+sudo  pacman -R fakeroot dkms binder_linux-dkms linux-neptune-611-headers
 sudo steamos-readonly enable
 
 #./steamos-waydroid-installer.sh
