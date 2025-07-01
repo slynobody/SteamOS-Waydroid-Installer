08130c0ffba5eda737aedea1431bdd16c3417dfe
slynobody <55579266+slynobody@users.noreply.github.com>
2025-07-01 13:51:23 +0000

    Update build_stable.sh

--- build_stable.sh
+++ build_stable.sh
@@ -19,8 +19,8 @@ sudo -s pacman -S fakeroot dkms debugedit --noconfirm --overwrite "*"
 
 
 #get & install latest headers, 'main'-context
-sudo wget https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-3.7/os/x86_64/linux-neptune-611-headers-6.11.11.valve14-1-x86_64.pkg.tar.zst
-sudo -s pacman -U ./linux-neptune-611-headers-6.11.11.valve14-1-x86_64.pkg.tar.zst --noconfirm --overwrite "*"
+sudo wget https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-3.7/os/x86_64/linux-neptune-611-headers-6.11.11.valve19-1-x86_64.pkg.tar.zst
+sudo pacman -U ./linux-neptune-611-headers-6.11.11.valve19-1-x86_64.pkg.tar.zst --noconfirm --overwrite "*"
 
 #get & install latest binder-dkms (which builds the missing kernel-module)
 git clone https://aur.archlinux.org/binder_linux-dkms.git
@@ -30,9 +30,9 @@ makepkg
 sudo -s pacman -U ./binder_linux-dkms-6.14-1-x86_64.pkg.tar.zst --noconfirm --overwrite "*"
 
 cd ..
-mkdir binder/6.11.11-valve14-1-neptune-611 &> /dev/null
+mkdir binder/6.11.11-valve19-1-neptune-611 &> /dev/null
 #offer the kernel-module to the sd-waydroid-installer (or manually adapt to include 3.7 latest)
-sudo cp /usr/lib/modules/6.11.11-valve14-1-neptune-611-g96885212a919/updates/dkms/binder_linux.ko.zst binder/6.11.11-valve14-1-neptune-611 &> /dev/null
+sudo cp /usr/lib/modules/6.11.11-valve19-1-neptune-611-gg88b36d49a5e3/updates/dkms/binder_linux.ko.zst binder/6.11.11-valve19-1-neptune-611 &> /dev/null
 
 
 #remove old packages (nec for updating with higher kernel version later on)
