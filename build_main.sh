a3651585054a83e8b68fb466c29dffcf3eb79abc
slynobody <55579266+slynobody@users.noreply.github.com>
2025-07-04 07:18:53 +0000

    Update build_main.sh

--- build_main.sh
+++ build_main.sh
@@ -3,8 +3,8 @@ sudo steamos-devmode enable --no-prompt
 sudo pacman -S fakeroot dkms dnsmasq lxc debugedit --noconfirm --overwrite "*"
 
 #get & install latest headers, 'main'-context
-sudo wget https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-main/os/x86_64/linux-neptune-611-headers-6.11.11.valve19-1-x86_64.pkg.tar.zst
-sudo pacman -U ./linux-neptune-611-headers-6.11.11.valve19-1-x86_64.pkg.tar.zst  --noconfirm --overwrite "*"
+sudo wget https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-main/os/x86_64/linux-neptune-611-headers-6.11.11.valve20-1-x86_64.pkg.tar.zst
+sudo pacman -U ./linux-neptune-611-headers-6.11.11.valve20-1-x86_64.pkg.tar.zst  --noconfirm --overwrite "*"
 
 #get & install latest binder-dkms (which builds the missing kernel-module)
 git clone https://aur.archlinux.org/binder_linux-dkms.git
@@ -13,9 +13,9 @@ makepkg
 sudo pacman -U ./binder_linux-dkms-6.14-1-x86_64.pkg.tar.zst --noconfirm --overwrite "*"
 
 cd ..
-mkdir binder/6.11.11-valve19-1-neptune-611 &> /dev/null
+mkdir binder/6.11.11-valve20-1-neptune-611 &> /dev/null
 #offer the kernel-module to the sd-waydroid-installer (one has always to adapt the script to include 3.8 latest)
-sudo cp /usr/lib/modules/6.11.11-valve19-1-neptune-611-g88b36d49a5e3/updates/dkms/binder_linux.ko.zst binder/6.11.11-valve19-1-neptune-611 &> /dev/null
+sudo cp /usr/lib/modules/6.11.11-valve20-1-neptune-611-gd35c3ed359a0/updates/dkms/binder_linux.ko.zst binder/6.11.11-valve20-1-neptune-611 &> /dev/null
 
 #remove now unnec packages (nec for future reinstalls!)
 sudo  pacman -R fakeroot dkms binder_linux-dkms linux-neptune-611-headers --noconfirm --overwrite "*"
